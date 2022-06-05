f7eada8019ce67427bfd289f9408ffe46577b8b6
hritsh <hritish.mahajan@gmail.com>
2022-06-05 12:03:02 +0400

    Functional App Done with Basic CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emojify';
+  inputText = "";
+  output = "";
+  emojiMap: { [key: string]: string } = {
+    "a": "🅰️",
+    "b": "🅱️",
+    "c": "©️",
+    "d": "🄳",
+    "e": "📧",
+    "f": "🄵",
+    "g": "🄶",
+    "h": "♓️",
+    "i": "ℹ️",
+    "j": "🄹",
+    "k": "🄺",
+    "l": "🄻",
+    "m": "♏️",
+    "n": "♑️",
+    "o": "🅾️",
+    "p": "🅿️",
+    "q": "🅀",
+    "r": "®️",
+    "s": "💲",
+    "t": "✝️",
+    "u": "⛎",
+    "v": "♈️",
+    "w": "🅆",
+    "x": "❎",
+    "y": "🅈",
+    "z": "🅉",
+    "A": "🅰️",
+    "B": "🅱️",
+    "C": "©️",
+    "D": "🄳",
+    "E": "📧",
+    "F": "🄵",
+    "G": "🄶",
+    "H": "♓️",
+    "I": "ℹ️",
+    "J": "🄹",
+    "K": "🄺",
+    "L": "🄻",
+    "M": "♏️",
+    "N": "♑️",
+    "O": "🅾️",
+    "P": "🅿️",
+    "Q": "🅀",
+    "R": "®️",
+    "S": "💲",
+    "T": "✝️",
+    "U": "⛎",
+    "V": "♈️",
+    "W": "🅆",
+    "X": "❎",
+    "Y": "🅈",
+    "Z":"🅉"
+  };
+
+  onInput(text: string) {
+    this.output = this.emojiReplace(text)
+  }
+
+  emojiReplace(text: string): string {
+    let result = text.replace(/\w/gi, m => this.emojiMap[m]);
+    return result;
+  }
+
+  copyMessage() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.output;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    document.getElementById("outputHeading")!.innerHTML = "Copied!";
+    setTimeout(function(){
+        document.getElementById('outputHeading')!.innerHTML = "Output (Click to Copy)";
+    }, 2000);
+  }
+  
 }
